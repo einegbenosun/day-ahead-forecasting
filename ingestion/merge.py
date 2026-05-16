20d0aa5f347d03bdcd8918e72cc85cdb1f5ccb9f
Eddy <einegbenosun@gmail.com>
2026-05-16 09:57:02 +0100

    further cleaning of merged data

--- ingestion/merge.py
+++ ingestion/merge.py
@@ -1,6 +1,9 @@
+from genericpath import isfile
+
 import pandas as pd
 from pathlib import Path
 import csv 
+from datetime import datetime
 
 
 cams_path = Path(__file__).with_name("cams_cleaned.csv")
@@ -11,5 +14,26 @@ open_meteo_path = Path(__file__).with_name("open_meteo_cleaned.csv")
 open_meteo_df = pd.read_csv(open_meteo_path)
 
 merged_df = pd.merge(open_meteo_df, cams_df, on="timestamp", how="inner")
+
+#https://www.geeksforgeeks.org/pandas/python-pandas-split-strings-into-two-list-columns-using-str-split/
+split = merged_df["timestamp"].str.split("T", n=1, expand=True)
+
+merged_df["Hour"] = split[1].str.split(":").str[0]
+merged_df["Date"] = split[0]
+merged_df["Date"] = pd.to_datetime(merged_df["Date"])
+merged_df = merged_df.drop(columns=["timestamp"])
+merged_df['YEAR'] = merged_df['Date'].dt.year
+merged_df['MONTH'] = merged_df['Date'].dt.month
+merged_df['DAY'] = merged_df['Date'].dt.day
+merged_df = merged_df.drop(columns=["Date"])
+
+
+
+#https://stackoverflow.com/questions/74817120/convert-object-to-int-pandas-dataframe
+merged_df["Hour"] = pd.to_numeric(merged_df["Hour"], errors="coerce")
+#merged_df = pd.read_csv(merged_df, parse_dates=["timestamp"])
+
+
+print(merged_df.info())
 merged_df.to_csv("merged_data.csv", index=False)
-print(merged_df.columns.tolist())
+
--- training/training.py
+++ training/training.py
@@ -1,8 +1,21 @@
 import csv
 import pandas as pd
 import pathlib as path
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from matplotlib import pyplot as plt
+import numpy as np
+
+
 
 df_path = path.Path(__file__).with_name("merged_data.csv")
 df = pd.read_csv(df_path)
 
-print(df.head())
+pv_target = df["cams_ghi"]
+
+X_train, X_test, y_train, y_test = train_test_split(df.drop(["cams_ghi"], axis="columns"),pv_target, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+
